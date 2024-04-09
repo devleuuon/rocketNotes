05e0102a1283048eb02bd6e8456d2230b972b330
Lennon Fonseca <Lennonfonsecaa@gmail.com>
2024-04-09 11:25:51 -0300

    passwordMatched created

--- src/controllers/sessionsController.js
+++ src/controllers/sessionsController.js
@@ -1,5 +1,6 @@
-const knex = require('../database/knex')
-const AppError = require('../utils/appError')
+const knex = require('../database/knex') //conexão com o banco de dados
+const AppError = require('../utils/appError') // mostrar os erros
+const { compare } = require('bcryptjs') // vai comparar senhas criptografadas
 
 class SessionsController {
     async create(request, response) {
@@ -8,6 +9,12 @@ class SessionsController {
         const user = await knex('users').where({ email }).first()  //vai buscar no banco de dados se o email existe.
 
         if(!user) {
+            throw new AppError('E-mail e/ou senha incorreta', 401)
+        }
+
+        const passwordMatched = await compare(password, user.password) // vai comparar se senha digitada confere com a sehha que está no banco de dados.
+
+        if(!passwordMatched) {
             throw new AppError('E-mail e/ou senha incorreta', 401)
         }
 
