d2ca3aecd89b6c6f7bfaae7704343fbcb522e66c
Lennon Fonseca <Lennonfonsecaa@gmail.com>
2024-04-10 14:53:15 -0300

    add middleware in tags

--- src/routes/tags.routes.js
+++ src/routes/tags.routes.js
@@ -2,13 +2,14 @@
 
 const { Router } = require('express') //importa do 'express'
 const TagsController = require('../controllers/tagsControllers')
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated.js')
 
 const tagsRoutes = Router() //inicia projeto
 
 
 const tagsController = new TagsController()
 
-tagsRoutes.get('/:user_id', tagsController.index)
+tagsRoutes.get('/', ensureAuthenticated, tagsController.index)
 
 
 module.exports = tagsRoutes //exportando o código
