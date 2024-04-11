c3876f5d7cda2bd41fa91f0d009d2cc5bfe08fc8
Lennon Fonseca <Lennonfonsecaa@gmail.com>
2024-04-11 11:13:19 -0300

    diskStorage created

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -1,4 +1,24 @@
 const path = require('path')
+const multer = require('multer') // para fzer o upload
+const crypto = require('crypto') // vai gerar um hash de forma aleatória, criando diferentes nomes para os rquivos, evitando de um arquivo sobrepor outro com o mesmo nome.
 
 const TMP_FOLDER = path.resolve(__dirname, '..', '..', 'tpm')
 const UPLOADS_FOLDER = path.resolve(__dirname, 'uploads')
+
+const MULTER = {
+    storage: multer.diskStorage({
+        destination: TMP_FOLDER,
+        filename(request, file, callback) {
+            const fileHash = crypto.randomBytes(10).toString('hex') //criaando nome aleatório em string e hexadecimal.
+            const fileName = `${fileHash}-${file.originalname}` //hash + nome do arquivo original.
+
+            return callback(null, fileName)
+        }
+    })
+}
+
+module.exports = {
+    TMP_FOLDER,
+    UPLOADS_FOLDER,
+    MULTER
+}
---
+++ src/providers/diskStorage.js
@@ -0,0 +1,28 @@
+const fs = require('fs') //para manipular arquivos.
+const path = require('path')
+const uploadConfig = require('../configs/upload')
+
+class DiskStorage {
+    async saveFile(file) {
+        await fs.promises.rename( //não vai renomear nome, vai trocar os arquivos de pasta.
+            path.resolve(uploadConfig.TMP_FOLDER, file),
+            path.resolve(uploadConfig.UPLOADS_FOLDER, file)
+        )
+
+        return file
+    }
+
+    async deleteFile(file) {
+        const filePath = path.resolve(uploadConfig.UPLOADS_FOLDER, file) //buscando arquivo para deletar.
+
+        try { //vai verificar o status do arquivo, se ele existe ou tem algum problema
+            await fs.promises.stat(filePath)
+        } catch {
+            return
+        }
+
+        await fs.promises.unlink(filePath) //vai deletar o arquivo.
+    }
+}
+
+module.exports = DiskStorage
